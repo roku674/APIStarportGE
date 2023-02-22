3ee14c0b8c91ffb5e9ec0364e46f01a4e73c5432
roku674 <roku674@gmail.com>
2023-02-22 08:50:41 -0500

    Create AnalyticsController.cs
    Moved to its own controller

---
+++ APIStarportGE/Controllers/AnalyticsController.cs
@@ -0,0 +1,42 @@
+﻿using APIAccount.Models;
+using APIStarportGE.Filters;
+using Microsoft.AspNetCore.Mvc;
+using Optimization.Objects;
+using System.Collections.Generic;
+
+namespace APIStarportGE.Controllers
+{
+    [APIKey]
+    [Route("api/analysis")]
+    [ApiController]
+    public class AnalyticsController : ControllerBase
+    {
+
+        [HttpGet("getbuilds")]
+        public ActionResult GetBuilds(string server)
+        {
+            string database = Settings.Configuration[$"MongoDB:Databases:{server}"];
+
+            if (string.IsNullOrEmpty(database))
+            {
+                return BadRequest($"{server} was not a valid server!");
+            }
+            ColonyModel colonyModel = new ColonyModel(database);
+
+            List<string> builds = colonyModel.GetBuildables();
+
+            if (builds.Count > 0)
+            {
+                return Ok(builds);
+            }
+            else if (builds.Count == 0)
+            {
+                return StatusCode(404, $"No planet were found!");
+            }
+            else
+            {
+                return StatusCode(503);
+            }
+        }
+    }
+}
